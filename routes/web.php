46469e5303a948b219198b4b212386644b40be8b
JobJo303 <phakorn5149@gmail.com>
2024-09-05 16:58:50 +0700

    Adding project to GitHub repo

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/firstPage',function (){
+Route::get('/',function (){
     return view(('firstPage'));
 });
 
